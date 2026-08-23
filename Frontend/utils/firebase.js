e6009057aa8101d25ed0797d178ad94d4ff33172
dharmi11 <dharmpreetbhinder9@gmail.com>
2026-08-23 18:31:49 +0530

    fix firebase production environment

--- Frontend/utils/firebase.js
+++ Frontend/utils/firebase.js
@@ -7,14 +7,23 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
+//   authDomain: "codexai-c9da4.firebaseapp.com",
+//   projectId: "codexai-c9da4",
+//   storageBucket: "codexai-c9da4.firebasestorage.app",
+//   messagingSenderId: "856735059173",
+//   appId: "1:856735059173:web:7698796db4c9874858980c",
+//   measurementId: "G-E3FC637N1W"
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
-  authDomain: "codexai-c9da4.firebaseapp.com",
-  projectId: "codexai-c9da4",
-  storageBucket: "codexai-c9da4.firebasestorage.app",
-  messagingSenderId: "856735059173",
-  appId: "1:856735059173:web:7698796db4c9874858980c",
-  measurementId: "G-E3FC637N1W"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
