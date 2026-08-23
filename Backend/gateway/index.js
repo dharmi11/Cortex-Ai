d99c760d250f3681245f304fd812c6bdf38fc492
dharmi11 <dharmpreetbhinder9@gmail.com>
2026-08-23 17:41:00 +0530

    new changes

--- Backend/gateway/index.js
+++ Backend/gateway/index.js
@@ -36,7 +36,7 @@ app.use("/api/user" , protect , getCurrentUser);
 
 
 app.get("/" , (req,res)=>{
-    res.send("gateway is working afte Ci/Cd pipeline v1 ")
+    res.send("gateway is working afte Ci/Cd pipeline v2 ")
 })
 
 app.listen(port , ()=>{
