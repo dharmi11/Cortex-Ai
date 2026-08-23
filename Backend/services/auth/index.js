5cb11a8bc614e81fb19beac62c25a2c8c81042ed
dharmi11 <dharmpreetbhinder9@gmail.com>
2026-08-23 17:15:33 +0530

    new

--- Backend/services/auth/index.js
+++ Backend/services/auth/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
     res.json({
-        message: "Hello from from Auth"
+        message: "Hello from from Auth v1"
     })
 })
 
